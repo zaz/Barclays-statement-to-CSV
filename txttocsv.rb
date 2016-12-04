81ae889270048fca5b80091f02119b924233c7f6
Zaz Brown <zazbrown@zazbrown.com>
2016-12-04 11:31:31 -0500

    * conversion script

--- txttocsv.rb
+++ txttocsv.rb
@@ -1,185 +1,49 @@
 #!/usr/bin/env ruby
 require 'date'
-require 'pdf-reader'
 
-def barclays_parse(lines)
-	return enum_for :barclays_parse, lines unless block_given?
-
-	for line in lines
-		case line
-		when :new_file
-			go = false
-			data = {}
-		when ""
-			# puts  # XXX DEBUG
-		when /^Date  *Description/
-			go = true
-			# puts line  # XXX DEBUG
-		when /^(\d\d? [A-Z][a-z]{2})?  *(\S.*?)     *([0-9,]+\.\d\d)(     *[0-9,]+\.\d\d)?/
-			yield data unless data.empty?
-			# XXX: Fix dates, they currently default to THIS year
-			data[:date] = $1 ? Date.parse($1) : nil
-			data[:description], data[:ref] = $2, nil
-			data[:transaction], data[:balance] = [ $3, $4 ].map{ |e| e.gsub(",", "").to_f if e }
-			if data[:description] =~ /^((Start|End) balance|Balance)$/
-				go = $2 == "Start"
-				# Blank sheets have only "Balance", and will result in 2 :ends in a row
-				data[:description] = go ? :start : :end
-				data[:balance] = data[:transaction]
-				data[:transaction] = nil
-			end
-			yield data unless go
-			# puts "\e[33m#{line}\e[m"  # XXX DEBUG
-		when /( * Ref: )(.*?)(      .*|$)/
-			data[:ref] = $2
-			# puts "#{$1}\e[32m#{$2}\e[m#{$3}"  # XXX DEBUG
-			# END of page
-		when / {20} *Continued/
-			go = false
-			yield data
-			# puts line  # XXX DEBUG
-			# DESCRIPTION  # XXX Exclude lines indented by >20 spaces?
-		when /(^ {0,40})(\S.*?)( {7}|$)/
-			if go
-				data[:description] << " " + $2
-				# puts "#{$1}\e[32m#{$2}\e[m"  # XXX DEBUG
-			else
-				# puts "#{$1}#{$2}"  # XXX DEBUG
-			end
-		else
-			# puts "\e[1;31m#{line}\e[m"  # XXX DEBUG
-		end
+info = []
+amounts = []
+op = :preface
+for line in ARGF do
+	case line
+	when "Balance\n"
+		op = :transactions
+	when /^\d\d? [A-Z][a-z]{2}$/
+		info << [ Date.parse( line ) ]
+	when /^(\d\d? [A-Z][a-z]{2}) (.*)$/
+		next if line =~ /^(\d\d [A-Z][a-z]{2}) \d{4} /
+		match = /^(\d\d [A-Z][a-z]{2}) (.*)$/.match(line)
+		info << [ Date.parse( match[1] ), match[2] ]
+	when /^\d+.\d\d$/
+		amounts << line.to_f
+	else
+		info.last << line if op == :transactions
+		op = :end if line == "End balance\n"
 	end
 end
 
-def readlines(files)
-	return enum_for :readlines, files unless block_given?
+transactions = amounts.select.each_with_index{ |a, i| i.odd? }
+balances = amounts.select.each_with_index{ |a, i| i.even? }
 
-	files.each do |file|
-		# puts "\e[1m##### #{file} #####\e[m"  # XXX DEBUG
-		File.open(file, "rb") do |io|
-			begin
-				reader = PDF::Reader.new(io)
-			rescue PDF::Reader::MalformedPDFError
-				next
-			end
-			yield :new_file
-			reader.pages[0..-2].map{ |page| page.text.split("\n") }.flatten.each do |line|
-				yield line
-			end
-		end
-	end
+calculated_transactions = balances.each_cons(2).map do |p, n|
+	n - p
 end
 
-def check(output)
-	return enum_for :check, output unless block_given?
+# Verify final balance:
+raise "Couldn't verify final balance." unless transactions.last == balances.last
 
-	yield output.first
-	output.each_cons(2) do |a, b|
-		printf "A "
-		yield a
-		printf "  "
-		yield b
-		next
-		b[:date] = a[:date] unless b[:date]
-		case b[:description]
-		when :start
-			abort ":start not preceded by :end" unless a[:description] == :end
-			abort "start balance seems to be a transaction" if b[:transaction]
-			abort "start balance != previous end balance" if b[:balance] != a[:balance]
-		when :end
-			abort "end balance seems to be a transaction" if b[:transaction]
-			abort "end balance != previous balance" if b[:balance] != a[:balance]
-		else
-			abort "no amount specified for transaction" unless b[:transaction]
-			if b[:balance] and a[:balance]
-				difference = b[:balance] - a[:balance]
-				puts difference
-				if b[:transaction] == difference.abs
-					b[:transaction] = difference
-				else
-					abort "transaction doesn't match difference in balance"
-				end
-			end
-		end
-	end
+# Verify that transactions and balances match:
+calculated_transactions.each_with_index do |t, i|
+	raise "Transaction #{i} does not balance." unless t.abs == transactions[i]
 end
 
-# barclays_parse(readlines(ARGV)).each_cons(2) do |a, i|
-bp = []
-barclays_parse(readlines(ARGV)) do |i|
-	bp << i
-	bp.each do |x|
-		print x[:date].to_s[-4,5].sub('-', '') if x[:date]
-		print ' ', x[:transaction] if x[:transaction]
-		case x[:description]
-		when :start; print '!'
-		when :end; print '|'
-		else; print ')'
-		end
-	end
-	puts
+calculated_transactions.unshift(nil)
+info.map!.each_with_index do |t, i|
+	[ t[0], calculated_transactions[i], t[1..-1].join("").gsub("\n", " ") ]
 end
-exit
-puts "-----"
-bp.each do |x|
-	puts x
-end
-puts bp.length
-exit
-# check(barclays_parse(readlines(ARGV))).each_cons(2) do |i|
-check(bp).each do |i|
+
+# Output:
+info.each do |i|
 	puts i
-	next
-	date = i[:date] ||= " " * 10
-	transaction = i[:transaction] ? "% 10.2f" % i[:transaction] : " " * 10
-	balance = i[:balance] ? "% 10.2f" % i[:balance] : " " * 10
-	puts "\e[33m%s\t%s\t%s\t%s\t%s\e[m" % [ date, transaction, balance, i[:ref], i[:description] ]
+	puts "----"
 end
-
-# 	transactions = amounts.select.each_with_index{ |a, i| i.odd? }
-# 	balances = amounts.select.each_with_index{ |a, i| i.even? }
-
-# 	calculated_transactions = balances.each_cons(2).map do |p, n|
-# 		n - p
-# 	end
-
-# 	begin
-# 		# Verify final balance:
-# 		raise "Couldn't verify final balance." unless transactions.last == balances.last
-
-# 		# Verify that transactions and balances match:
-# 		calculated_transactions.each_with_index do |t, i|
-# 			unless t.abs == transactions[i].abs
-# 				raise "Transaction #{i} does not balance."
-# 			end
-# 		end
-
-# 	rescue RuntimeError
-# 		puts "From sheet:"
-# 		amounts.each_with_index do |a, i|
-# 			type = i.even? ? "balance" : "transaction"
-# 			puts "\t#{a} (#{type})"
-# 		end
-# 		puts "Calculated transactions:"
-# 		puts calculated_transactions.map{ |t| "\t" + t.to_s }
-# 		raise
-# 	end
-
-# 	calculated_transactions.unshift(nil)
-# 	info.map!.each_with_index do |t, i|
-# 		[ t[0], calculated_transactions[i], t[1..-1].join("").gsub("\n", " ") ]
-# 	end
-
-# 	# Output:
-# 	info.each do |i|
-# 		if i[1]
-# 			puts "%s   % 10.2f   %s" % [ i[0], i[1], i[2] ]
-# 		else
-# 			puts "%s                %s" % [ i[0], i[2] ]
-# 		end
-# 		# puts i
-# 		# puts "----"
-# 	end
-
-# end
